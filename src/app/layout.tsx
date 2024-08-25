34edfde2bd7cbb876b2e48d9974a20f5c0a592f9
Amar <jeetamar496@gmail.com>
2024-08-25 12:38:56 +0530

    viewport added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+
       <body className={inter.className}>
         <StoreProvider>
 
